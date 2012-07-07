36c579a4c8e1a5392c9befeba1f15fa69e504ac3
Justin Weidner <shadeban@gmail.com>
2012-07-06 22:46:28 -0500

    'Validation!'

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    @update = {
+        title:       'Lorem Ipsum',
+        description: 'Wibbles are fun!',
+        image_url:   'lorem.jpg',
+        price:       19.95
+    }
   end
 
   test "should get index" do
@@ -18,8 +24,8 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
-    end
+      post :create, product: @update
+  end
 
     assert_redirected_to product_path(assigns(:product))
   end
@@ -35,7 +41,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    put :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+    put :update, id: @product, product: @update
     assert_redirected_to product_path(assigns(:product))
   end
 
