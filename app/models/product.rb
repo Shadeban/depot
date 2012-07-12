c00330b2cb6a28f2d8c7efd1a468aa53c1ed1fe5
Justin Weidner <shadeban@gmail.com>
2012-07-11 22:26:37 -0500

    before counter on index

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true, length: {minimum: 10, message:"MAKE MORE BIGGAR NEEDS TEN"}
@@ -8,4 +10,18 @@ class Product < ActiveRecord::Base
       message: 'must be a URL for GIF, JPG or PNG image.'
   }
 
-end
+
+  private
+    
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+   
+    
+    end
+  end
+
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,6 +15,7 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    
   end
 
   test "should get new" do
